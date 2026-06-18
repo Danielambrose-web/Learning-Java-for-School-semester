11c97034435bd4f3358ffbfc7f75b6c97295365f
Danielambrose-web <danielessientech121@gmail.com>
2026-06-18 18:18:52 +0100

    day 1

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*int number = 10;
+        Double trans = 12.3;
+        char name = 'A';
+        System.out.println(number);
+        System.out.print(trans);
+        System.out.print(name);*/
+        
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+       int age = scanner.nextInt();
+
+        scanner.close();
+        //System.out.println("My name is " + userName + " i am "+ age);
+          if(age < 10){
+            System.out.println("my name is  " + userName);
+          };*/
+    }
+}
