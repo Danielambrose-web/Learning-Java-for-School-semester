156442e9e162a846dedb7016ad067cd6e8ebff71
Danielambrose-web <danielessientech121@gmail.com>
2026-06-20 21:48:14 +0100

    started by declaring what i will need

---
+++ src/madLib.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class madLib {
+    public static void main(String[] args) {
+
+        String place = "";
+        String noun = "";
+        String  discription= "";
+        String action = "";
+        String mood = "";
+
+
+
+        // System.out.print(name);
+    }
+}
