93e30e356880cbfd5f07503ef213347f2025e5a0
Danielambrose-web <danielessientech121@gmail.com>
2026-06-21 23:55:04 +0100

    operators in java

---
+++ src/operators.java
@@ -0,0 +1,19 @@
+public class operators {
+    public static void main(String[] args) {
+
+    //    int x =10;
+    //    int y = 3;
+
+
+    //    int a = x/y;
+
+       double x =10;
+       double y = 3;
+
+
+    //    double a = x/y;
+
+    //    x--;
+        System.out.println(x);
+    }
+}
